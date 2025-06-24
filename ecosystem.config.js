0e308774ed5ffbc951a17da661dd5ab440bbbbeb
Brian Gates <brian.g.gates@gmail.com>
2025-06-23 23:44:34 -0700

    feat: update emoji handling and add new dice images
    - Added new emoji images for various dice types in the media/dice/emoji directory.
    - Updated the emoji upload functionality to check for existing emojis and delete them before uploading new ones.
    - Enhanced error handling during emoji upload to provide clearer feedback on failures.
    - Refactored image reading to use base64 encoding for emoji uploads.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,14 @@ module.exports = {
   apps: [
     {
       name: "bot",
-      script: "src/bot/index.ts",
-      interpreter: "deno",
+      script: "deno",
+      args: "task bot",
+      interpreter: "none",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
-      interpreter_args:
-        "--allow-env --allow-net --allow-read --allow-write --allow-run --allow-ffi --allow-import=unpkg.com,deno.land",
       env: {
         DENO_ENV: "production",
         DATABASE_URL: "postgres://postgres:postgres@localhost:5444/adventure",
       },
-      watch: false,
-      exp_backoff_restart_delay: 1000,
     },
     {
       name: "prisma-studio",
